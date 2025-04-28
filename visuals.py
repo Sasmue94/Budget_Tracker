8957d80d1ff87c533a8598b2635e4db51097b155
Sasmue94 <sascha@mueller1000.de>
2025-04-28 11:28:38 +0200

    added missing type annotations

--- visuals.py
+++ visuals.py
@@ -4,8 +4,8 @@ from streamlit import success, error, container
 from entries import Entry
 
 # render empty space specified in number of pixels
-def empty_space(px = 0):
-    return container(height=px, border=False)
+def empty_space(px = 0) -> None:
+    container(height=px, border=False)
 
 # displays an entry based onits type
 def display(entry: Entry) -> None:
