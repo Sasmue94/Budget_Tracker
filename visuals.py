fa6d1eaa9a52ab0555758e99c0627f330882cff6
Sasmue94 <sascha@mueller1000.de>
2025-04-28 11:26:05 +0200

    import error in visuals.py fixed

--- visuals.py
+++ visuals.py
@@ -1,11 +1,11 @@
 # basic module to show things on screen
 
-from streamlit import success, error
+from streamlit import success, error, container
 from entries import Entry
 
 # render empty space specified in number of pixels
 def empty_space(px = 0):
-    return st.container(height=px, border=False)
+    return container(height=px, border=False)
 
 # displays an entry based onits type
 def display(entry: Entry) -> None:
